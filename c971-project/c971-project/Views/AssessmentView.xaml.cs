4d70f16ff0f043437ee3f7d8174b90d03d9f5081
Trey Hall <hallta3199@gmail.com>
2023-04-17 12:14:16 -0400

    updates

---
+++ c971-project/c971-project/Views/AssessmentView.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace c971_project.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AssessmentView : ContentView
+    {
+        public AssessmentView()
+        {
+            InitializeComponent();
+        }
+    }
+}
